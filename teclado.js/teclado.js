827db6879c0556050c60438011cf33af4185a0ca
Joao Wasconcelos <joaowasconcelos18@gmail.com>
2023-05-22 19:03:46 -0300

    teclado virtual

---
+++ teclado.js/teclado.js
@@ -0,0 +1,7 @@
+function aperta(lt) {
+    document.getElementById("aperta").innerHTML = "Você apertou a tecla " + lt
+}
+
+function solta(lt) {
+    document.getElementById("solta").innerHTML = `Você soltou a tecla ${lt}`
+}
